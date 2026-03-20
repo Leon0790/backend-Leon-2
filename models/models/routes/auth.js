e4f94d8e89b363b2b69d6bf05fcb36d991a4a642
Leon0790 <113593364+Leon0790@users.noreply.github.com>
2026-03-20 14:25:34 +0300

    Update auth.js

--- models/models/routes/auth.js
+++ models/models/routes/auth.js
@@ -5,3 +5,11 @@ router.post('/signup', signup);
 router.post('/login', login);
 
 module.exports = router;
+
+git add routes/auth.js
+git add routes/students.js
+git add controllers/authController.js
+git add controllers/studentController.js
+git add middleware/auth.js
+git add models/User.js
+git add models/Student.js
